8d476df625e28d8eb02cfdbb402d19291d1270ef
Andy Meneely <andy@se.rit.edu>
2019-05-29 15:00:21 -0400

    quick tweak

---
+++ samples/saves/_save_scaled.rb
@@ -0,0 +1,16 @@
+# require 'squib'
+require_relative '../../lib/squib'
+
+Squib::Deck.new(width: 50, height: 20) do
+  background color: :gray
+  text str: 'Text!', font: 'Sans 6'
+
+  save_png prefix: 'save_png_no_scaled_'
+  save_png prefix: 'save_png_no_scaled_trimmed_', trim: 5, trim_radius: 5
+  # save_png supports scaling to a different size
+  save_png width: 100, height: 40, prefix: 'save_png_scaled_'
+  save_png width: 100, height: 40, trim: 5, trim_radius: 5,
+           prefix: 'save_png_scaled_trimmed_'
+  save_png width: 100, height: 40, trim: 5, trim_radius: 5,
+           rotate: :clockwise, prefix: 'save_png_scaled_trimmed_rotated_'
+end
